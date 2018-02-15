e90265f348631c966d155da747a0ca131a7a1902
Tanvir Sarker <tanvir603@gmail.com>
2018-02-15 22:21:16 +0600

    loader fix

--- src/app/service/content.service.ts
+++ src/app/service/content.service.ts
@@ -5,12 +5,10 @@ import { HttpClient } from "@angular/common/http";
 export class ContentService {
 
   constructor(private http: HttpClient) {}
-
   //server = 'http://localhost/myWebLaravel/public/api';
   server = 'http://tanvirpro.com/all_project/api/public/api';
 
   getData(section){
     return this.http.get(this.server+section);
   }
-
 }
