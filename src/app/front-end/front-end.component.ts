b433ab17d40f54117d3dfb65c225fdc69a0592d2
Tanvir Sarker <tanvir603@gmail.com>
2018-02-17 08:30:47 +0600

    welcome msg modal in component

--- src/app/front-end/front-end.component.ts
+++ src/app/front-end/front-end.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ContentService} from "../service/content.service";
 
+declare var $: any;
+declare var introJs: any;
+
 @Component({
   selector: 'app-front-end',
   templateUrl: './front-end.component.html',
@@ -15,6 +18,64 @@ export class FrontEndComponent implements OnInit {
   ngOnInit(){
     this.content.getData('/welcome').subscribe((data: any) => {
       this.welcomeContent = data;
+      this.welcomeModal();
+    });
+  }
+
+  welcomeModal(){
+    $(function () {
+      $("#tutorial").iziModal({
+        title: 'Notification',
+        icon: 'fa fa-book',
+        radius: 10,
+        padding: 100,
+      });
+
+      $(".under-cons").iziModal({
+        title: 'Construction',
+        subtitle: 'On going...',
+        transitionIn: 'bounceInDown',
+        transitionOut: 'bounceOutDown',
+        transitionInOverlay: 'bounceInUp',
+        transitionOutOverlay: 'bounceOutUp',
+        icon: 'fa fa-cogs',
+        radius: 5,
+        padding: 20,
+        loop: true
+      });
+      setTimeout(function() {
+        $("#welcome-modal").iziModal({
+          title: 'Portfolio Page',
+          subtitle: 'My portfolio <strong>WebSite</strong>',
+          icon: 'fa fa-eercast',
+          width: 1000,
+          overlayClose: false,
+          radius: 5,
+          padding: 20,
+
+          onClosed: function(){
+            var lastElem;
+            introJs()
+              .setOption("overlayOpacity", 1)
+              .setOption("exitOnOverlayClick", false)
+              .setOption("exitOnEsc", false)
+              .onchange(function(targetElement) {
+                lastElem = targetElement;
+                $(targetElement).prop('disabled', true);
+                $(targetElement).prev().prop('disabled', false);
+
+                $(document).on('click', '.introjs-skipbutton', function () {
+                  $(targetElement).prop('disabled', false);
+                });
+              }).oncomplete(function() {
+              $(lastElem).prop('disabled', false);
+              $("#tutorial").iziModal('open');
+            }).start();
+          }
+        });
+
+        $("#welcome-modal").iziModal('open');
+      }, 1000);
     });
   }
 }
